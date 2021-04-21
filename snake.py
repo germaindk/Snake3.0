2ef3f7b107a01fc5acd66875b31e64b313ec4345
germaindk <55692792+germaindk@users.noreply.github.com>
2021-04-21 22:03:10 +0200

    Update snake.py

--- snake.py
+++ snake.py
@@ -4,14 +4,19 @@ import random
 import color
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+WIN_X = 800
+WIN_Y = 600
+screen = pygame.display.set_mode((WIN_X,WIN_Y))
 pygame.display.set_caption('SN Snake')
 
 def main():
 
 	CLOCK = pygame.time.Clock()
 	snake_pos=[200,70]
-	snake_body=[[200,70],[200-10,70],[200-(2*10),70]]
+	snake_body=[[200,70] , [190 , 70] , [180,70]]
+
+	egg_pos=[0,0]
+	egg_spawn = True
 	#direction du serpent au répart
 	direction = 'right'
 	#score
@@ -37,9 +42,9 @@ def main():
 
 		#désiner le cor du serpent
 		for square in snake_body:
-			pygame.draw.rect(screen ,(255, 255, 0), (square[0],square[1],10,10))
+			pygame.draw.rect(screen ,(0, 255, 0), (square[0],square[1],10,10))
 
-		# noinspection PyUnreachableCode
+		# mouvement
 		if direction == 'right':
 			snake_pos[0] += 10
 		elif direction == 'left':
@@ -50,8 +55,39 @@ def main():
 			snake_pos[1] += 10
 
 
+		if snake_pos[0] <=0 or snake_pos[0] >= WIN_X:
+			sys.exit()
+		if snake_pos[1] <=0 or snake_pos[1] >= WIN_Y:
+			sys.exit()
 
-		snake_body.pop(0)
+		for square in snake_body[1:]:
+			if pygame.Rect(square[0],square[1],10,10).colliderect(pygame.Rect(snake_pos[0],snake_pos[1],10,10)):
+				print("perdu")
+				pygame.display.quit
+				
+
+		if egg_spawn:
+			# choisire la posision de l’œuf
+			egg_pos = [random.randrange(40,WIN_X-40), random.randrange(40,WIN_Y-40)]
+			#desactiver le spawn les œuf
+			egg_spawn = False
+			# fair apraraitre l’œuf
+		pygame.draw.rect(screen ,(255,255,0),(egg_pos[0],egg_pos[1],10,10))
+		#detecter si le serpent touche la l'euf
+		if pygame.Rect(snake_pos[0],snake_pos[1],10,10).colliderect(pygame.Rect(egg_pos[0],egg_pos[1],10,10)):
+			egg_spawn = True
+			score += 10
+		else:
+			#agrandire la taille du serpent
+			snake_body.pop(0)
+
+
+		#aficher le score
+		font = pygame.font.SysFont('comicsans',40)
+		score_font = font.render(f'{score}' , True , (255,255,255))
+		font_pos = score_font.get_rect(center=(WIN_X//2-40 , 30))
+		screen.blit(score_font , font_pos)
+
 		snake_body.append(list(snake_pos))
 		pygame.display.update()
 		CLOCK.tick(25)
